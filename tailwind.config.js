2bd6ec68034ebb5aaaea7378b8f2ae5808fb2d9e
Ikenna Egwu <justaikay@gmail.com>
2021-02-22 17:53:02 +0000

    Before JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'mans': '40rem'
+        
+      }
+    },
   },
   variants: {
     extend: {},
